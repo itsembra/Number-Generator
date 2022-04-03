ef1656078c667ccda3c1495aeb78c7c6c2b12588
EmBra <noahbrauer5@gmail.com>
2022-04-03 19:40:13 +0200

    V.-1.0.0

---
+++ Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Generator
+{
+    public class RandomGenerator //Generator
+    {
+        private readonly Random _random = new Random();
+
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+    }
+}
